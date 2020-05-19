24588d23d9ec00b10a6ad880ac003f8068337ad7
jekyh0606 <jekyh0606@naver.com>
2020-05-19 19:29:51 +0900

    프로그래밍 기초 학습 결과 업로드

---
+++ 프로그래밍 학습/2015083054 전규효/기초/python/ex1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "아 파이썬 재미없다."

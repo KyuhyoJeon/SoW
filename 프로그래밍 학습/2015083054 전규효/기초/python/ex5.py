bcab0d6da92e39abb5247eb5d4d95bc8acf20fec
jekyh0606 <jekyh0606@naver.com>
2020-05-20 20:57:01 +0900

    프로그래밍 학습 결과 업로드

---
+++ 프로그래밍 학습/2015083054 전규효/기초/python/ex5.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+my_name = '규효'
+my_age = 27 #not a lie
+my_height = 171 # cm
+my_weight = 65 # kg
+my_eyes = '검정'
+my_teeth = '하양'
+my_hair = '갈색'
+
+print "%s에 대해 이야기 해 보죠." % my_name
+print "키는 %d 센티미터고요." % my_height
+print "몸무게는 %d 킬로그램이에요." % my_weight
+print "사실 아주 많이 나가는 건 아니죠."
+print "눈은 %s이고 머리는 %s이에요." % (my_eyes, my_hair)
+print "이는 보통 %s이고 커피를 먹으면 달라져요." % my_teeth
+
+# 이 줄은 까다롭지만 정확히 따라 하세요
+print "%d, %d, %d를 모두 더하면 %d 랍니다." % (my_age, my_height, my_weight, my_age + my_height + my_weight)

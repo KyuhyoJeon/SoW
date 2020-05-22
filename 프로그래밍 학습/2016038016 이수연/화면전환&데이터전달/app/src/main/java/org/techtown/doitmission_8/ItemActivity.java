7b2acbda6ea4f04c4ab6adabfa508036aac1b58b
dltndus5244 <dltndus5244@naver.com>
2020-05-22 10:07:25 +0900

    화면전환,데이터전달

---
+++ 프로그래밍 학습/2016038016 이수연/화면전환&데이터전달/app/src/main/java/org/techtown/doitmission_8/ItemActivity.java
@@ -0,0 +1,35 @@
+package org.techtown.doitmission_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+
+        Intent receivedIntent = getIntent();
+        String titleMsg = receivedIntent.getStringExtra("titleMsg");
+
+        Toast.makeText(this, "titleMsg : " + titleMsg, Toast.LENGTH_LONG).show();
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("message", "result message is OK!");
+
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+    }
+}
